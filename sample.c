b454c1e4b7d77a3237e7e30340edb9e6af9d9ca7
sathyadevops <sathyadevops1@gmail.com>
2021-05-11 14:09:13 +0000

    c-code

--- sample.c
+++ sample.c
@@ -1,6 +1,14 @@
+void getData()
+{
+  printf(" Hello! ");
+  printf(" Hello! ");
+  printf(" Hello! ");
+  printf(" Hello! ");
+}
 void main()
 {
   printf(" Hello world ");
   printf(" Hello world ");
   printf(" Hello world ");
 }
+
